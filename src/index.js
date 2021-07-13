ed54acf2d67e69116b8db1df22d8ca1a58b2b77e
mancjames <multicolouredsmurf@gmail.com>
2021-07-13 22:12:25 +0100

    added folders for styles and updated color variables in custom

--- src/index.js
+++ src/index.js
@@ -2,30 +2,29 @@
 // // Test import of an asset
 // import webpackLogo from '@/images/webpack-logo.svg'
 
-// Test import of styles
+// Import of styles
 import '@/styles/index.scss'
 
-// Appending to the DOM
-// const logo = document.createElement('img')
-// logo.src = webpackLogo
+// Import of bootstrap
+import 'bootstrap'
 
-function createElement({ name, attrs, content }) {
-  const el = document.createElement(name)
+// function createElement({ name, attrs, content }) {
+//   const el = document.createElement(name)
 
-  for (let i = 0; i < attrs.length; i++) {
-    const attr = attrs[i]
-    el.setAttribute(attr[0], attr[1])
-  }
+//   for (let i = 0; i < attrs.length; i++) {
+//     const attr = attrs[i]
+//     el.setAttribute(attr[0], attr[1])
+//   }
 
-  el.innerHTML = content
+//   el.innerHTML = content
 
-  return el
-}
+//   return el
+// }
 
-const helloWorld = createElement({
-  name: 'h2',
-  attrs: [['class', 'display-1']],
-  content: 'Hello world',
-})
+// const helloWorld = createElement({
+//   name: 'h2',
+//   attrs: [['class', 'display-1']],
+//   content: 'Hello world',
+// })
 
-document.body.appendChild(helloWorld)
+// document.body.appendChild(helloWorld)
