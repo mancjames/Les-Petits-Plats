0fa026d5e791c56c1da7eb3fee42a6fcb0a140a8
mancjames <multicolouredsmurf@gmail.com>
2021-09-14 00:27:29 +0100

    updated styling for cards

--- src/js/view/components/recipeCard.js
+++ src/js/view/components/recipeCard.js
@@ -19,7 +19,10 @@ RecipeCard.prototype.recipeCard = function () {
     { class: 'col mb-3' },
     createDom(
       'article',
-      { class: 'card bg-light h-100 text-black rounded border-0' },
+      {
+        class:
+          'card bg-light h-100 text-black rounded border-0 custom-card-height',
+      },
       createDom(
         'div',
         { class: 'card-img-top' },
@@ -27,20 +30,24 @@ RecipeCard.prototype.recipeCard = function () {
       ),
       createDom(
         'div',
-        { class: 'd-flex px-3 py-2 fw-bold' },
+        { class: 'd-flex px-3 py-2 fw-bold custom-title' },
         createDom('h5', `${this.name}`, {
-          class: 'card-title me-auto my-auto',
+          class: 'card-title me-auto my-auto col-8',
         }),
         createDom(
           'div',
-          { class: 'd-flex flex-row align-items-center' },
+          {
+            class: 'd-flex flex-row align-items-center justify-content-end',
+          },
           createDom('i', { class: 'far fa-clock' }),
-          createDom('p', `${this.time} min`)
+          createDom('p', { class: 'm-1' }, `${this.time} mins`)
         )
       ),
       createDom(
         'div',
-        { class: 'card-body d-flex flex-row flex-wrap font-small px-3 py-1' },
+        {
+          class: 'card-body d-flex flex-row flex-wrap font-small px-3 py-1',
+        },
         createDom(
           'div',
           { class: 'card-text d-flex flex-column w-50' },
@@ -60,7 +67,9 @@ RecipeCard.prototype.recipeCard = function () {
         createDom(
           'div',
           { class: 'card-text d-flex flex-column w-50' },
-          createDom('p', `${this.description}`)
+          createDom('p', `${this.description}`, {
+            class: 'custom-truncate',
+          })
         )
       )
     )
